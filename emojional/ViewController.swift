7e048293e8d0b38b7305149c7aba08121fa24fd0
Apple <apple@Apples-MBP-63.attlocal.net>
2019-07-11 18:55:47 -0400

    Complete Iteration 2

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let fruit = [
+        "🍏" : "green apple",
+        "🍎" : "red apple",
+        "🍇" : "grapes",
+        "🍊" : "orange",
+        "🍋" : "lemon",
+        "🍒" : "cherries"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,8 +27,11 @@ class ViewController: UIViewController {
     @IBAction func showMessage(sender: UIButton) {
         // when clicked, pop-up appears in center of screen, from dictionary of recipes for fruit button selected
         
+        //stores info about which button user tapped
+        let selectedFruit = sender.titleLabel?.text
+        
         //makes the alert
-        let alertController = UIAlertController(title: "Sauteed Apples", message: "This is supposed to be a recipe of sauteed apples.", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "Sauteed Apples", message: fruit[selectedFruit!], preferredStyle: UIAlertController.Style.alert)
         
         //need to give alert an action so that user can tap button to make alert go away
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
